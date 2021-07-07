d468af4e0056a7debd8801ad85efe90aaf18cd67
manchixue <xuemanchi258@126.com>
2021-07-07 23:39:54 +0800

    feat(Icon): 添加icon组件

---
+++ src/packages/icon.vue
@@ -0,0 +1,30 @@
+<template>
+	<svg class="x-icon" aria-hidden="true">
+		<use :xlink:href="`#icon-${icon}`"></use>
+	</svg>
+</template>
+
+<script>
+	import '../styles/icon';
+	export default {
+		props: {
+			icon: {
+				type: String,
+				required: true
+			}
+		},
+		name: "x-icon",
+		data () {
+			return {};
+		},
+		methods: {}
+	}
+</script>
+
+<style lang="scss">
+	.x-icon {
+		width: 25px;
+		height: 16px;
+		vertical-align: middle;
+	}
+</style>
